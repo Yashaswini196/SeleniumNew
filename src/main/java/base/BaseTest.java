7dddcf8479a7eb2493e990bb57bbeaf594390b15
Yashaswini196 <singhyashi196@gmail.com>
2026-05-03 13:21:33 +0530

    Implementing Logging

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.Log;
 
 public class BaseTest {
 
@@ -11,14 +12,17 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp(){
+        Log.info("Starting WebDriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        Log.info("Navigating to the URL");
         driver.get("https://admin-demo.nopcommerce.com/login");
     }
 
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
+            Log.info("Closing WebDriver");
             driver.quit();
         }
     }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,15 +4,20 @@ import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void TestValidLogin(){
+        Log.info("Starting the Validation");
         LoginPage loginPage = new LoginPage(driver);
+        Log.info("Adding credentials");
         loginPage.EnterEmail("admin@yourstore.com");
         loginPage.EnterPassword("admin");
         loginPage.clickLoginButton();
+        Log.info("Login Successful");
+        Log.info("Verifying page Title");
         System.out.println("Title of the page is= "+  driver.getTitle());
 //        Assert.assertEquals(driver.getTitle(), "Welcome");
 
